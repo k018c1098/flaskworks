4c615e1aa4fb177d485d35efaeeb7cd8815fcac6
“k018c1098” <k018c1098@it-neec.jp>
2020-07-29 09:30:08 +0900

    6.1.image_upload

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = './static/uploads/'
 
 @app.route('/')
+def index():
+	return render_template('index.html')
 
-def hello_world():
-	return 'Hello World!'
+@app.route('/send', methods=['POST'])
+def send():
+	img_file = request.files['img_file']
+	img_file.save(app.config['UPLOAD_FOLDER'] + img_file.filename)
+	return '<p>画像' + img_file.filename + 'を送信しました</p>'
 
 if __name__=='__main__':
 	app.debug = True
